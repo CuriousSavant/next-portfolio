e807222d97bdd31f4041b5eff245ace336b309da
Craft187 <this_account_for_backupCode_[0019]>
2024-08-22 20:59:27 +0700

    Update rounded btn befor sm to md

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -67,9 +67,9 @@ const Navbar = () => {
         </div>
         <div className='text-black'>
           <button
-            className='border border-teal-500 text-teal-500 text-lg p-2 smooth font-semibold hidden md:flex items-center
-            focus:bg-teal-500 focus:text-black rounded-md
-            '
+            className='border border-teal-500 text-teal-500 text-lg p-2 smooth font-semibold 
+            hidden md:flex items-center focus:bg-teal-500 focus:text-black rounded-md
+            hover:bg-teal-500 hover:text-black'
             onClick={() => window.location.href = 'https://www.instagram.com/junior_dev175/'}>
             <BsInstagram className='mr-2' />
             Contact Me
